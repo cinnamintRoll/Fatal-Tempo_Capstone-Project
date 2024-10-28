d3b0bcd342764af689acbd86c1d0ff8a82854349
DESKTOP-VACFDVM\USER-PC <logdatkyle@gmail.com>
2024-10-29 01:46:24 +0800

    Game Demo
    - Modified skybox to not have compression
    - fixed arcade buttons to not activate when the generator is not on yet
    - created Level Select screen
    - created Player Dodge Level Scene

--- Assets/Scripts/VRSelectAndBackHandler.cs
+++ Assets/Scripts/VRSelectAndBackHandler.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 namespace BNG
@@ -7,20 +8,23 @@ namespace BNG
     public class VRButtonInputHandler : MonoBehaviour
     {
         public EventSystem eventSystem; // Reference to the EventSystem managing UI
-
+        [SerializeField] private GameObject LowPowerScreen;
 
         public void SelectButtonPress()
         {
-            // Handle the "Select" button press
-            GameObject currentSelected = eventSystem.currentSelectedGameObject;
-
-            if (currentSelected != null)
+            if (!LowPowerScreen.activeInHierarchy)
             {
-                // Check if the selected object is a button and trigger its click event
-                UnityEngine.UI.Button button = currentSelected.GetComponent<UnityEngine.UI.Button>();
-                if (button != null)
+                // Handle the "Select" button press
+                GameObject currentSelected = eventSystem.currentSelectedGameObject;
+
+                if (currentSelected != null)
                 {
-                    button.onClick.Invoke(); // Simulate button click
+                    // Check if the selected object is a button and trigger its click event
+                    UnityEngine.UI.Button button = currentSelected.GetComponent<UnityEngine.UI.Button>();
+                    if (button != null)
+                    {
+                        button.onClick.Invoke(); // Simulate button click
+                    }
                 }
             }
         }
