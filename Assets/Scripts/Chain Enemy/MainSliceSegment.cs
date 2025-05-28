9e78038edb58129a77b78b8fce25fab1fba5d2a3
DESKTOP-VACFDVM\USER-PC <logdatkyle@gmail.com>
2025-05-28 17:04:14 +0800

    Chain  note enemy
    - added enemy main slice segment
    - moved slice segment script
    - addded chain enemy prefab
    - edited spawnable prefab to include chain enemy

---
+++ Assets/Scripts/Chain Enemy/MainSliceSegment.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSliceSegment : MonoBehaviour
+{
+    private PlayerHealth PlayerHealth;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        PlayerHealth = PlayerHealth.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        PlayerHealth.KillEnemy(this.transform.position);
+    }
+}
