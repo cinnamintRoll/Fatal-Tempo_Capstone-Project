59fb12dd45ccc0928606a962797155a5c728968c
DESKTOP-VACFDVM\USER-PC <logdatkyle@gmail.com>
2025-06-14 17:40:11 +0800

    SFX Integrations
    - finalized main menu intro
    - modifined Spawnables to have AI manager to access the enemy inside the spawnable object

---
+++ Assets/Scripts/IntroManager.cs
@@ -0,0 +1,42 @@
+using BNG;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroManager : MonoBehaviour
+{
+    [SerializeField] private GameObject IntroMenu;
+
+    [SerializeField] private LocomotionManager LocomotionManager;
+
+    private string SkipKey = "GeneratorIsOn";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool GameStart = PlayerPrefs.GetInt(SkipKey) != 0;
+
+        if (GameStart) {
+            IntroMenu.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Movement Disabled");
+            LocomotionManager.enabled = false;
+        }
+    }
+
+    public void closeMenu()
+    {
+        LocomotionManager.ChangeLocomotion(LocomotionType.SmoothLocomotion, true);
+        IntroMenu?.SetActive(false);    
+    }
+
+
+    public void SkipIntro()
+    {
+        PlayerPrefs.SetInt(SkipKey, 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
