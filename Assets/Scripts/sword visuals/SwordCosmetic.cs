eac8ad3c5166e9dcaf780f5f337769728a7c9eec
DESKTOP-VACFDVM\USER-PC <logdatkyle@gmail.com>
2025-07-08 02:41:20 +0800

    weapon customization
    - added new sword models
    - added sword loader

---
+++ Assets/Scripts/sword visuals/SwordCosmetic.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SwordCosmetic : MonoBehaviour
+{
+    [Header("Cosmetic ID (for saving/loading)")]
+    public string cosmeticID;
+
+    [Header("Handle Alignment (Set this to where hand should grip)")]
+    public Transform handleTransform;
+}
